8846e2aab78fe43d2ff86223e2e239a810fe376e
ESMAILESMAIL <sokot121@chmail.ir>
2016-04-15 15:26:41 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {184018132},
+    sudo_users = {108800687},
     admin_users = {},
     disabled_channels = {}
   }
